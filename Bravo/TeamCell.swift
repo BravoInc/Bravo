4509ebf73ca9dc61eed9919e27ba71dfc6964b2b
Unum Sarfraz <anamsarfraz@gmail.com>
2016-11-13 16:11:18 -0800

    Added team search table view
    - Added redirect to main team creation view after search

---
+++ Bravo/TeamCell.swift
@@ -0,0 +1,26 @@
+//
+//  TeamCell.swift
+//  Bravo
+//
+//  Created by Unum Sarfraz on 11/12/16.
+//  Copyright © 2016 BravoInc. All rights reserved.
+//
+
+import UIKit
+
+class TeamCell: UITableViewCell {
+
+    @IBOutlet weak var teamNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
