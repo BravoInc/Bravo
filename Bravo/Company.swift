f94740185f69e9bf8dc9cd99fe8dc197d787b1ea
Patwardhan, Saurabh <Saurabh.Patwardhan@capitalone.com>
2016-11-18 08:04:06 -0800

    Team creation on parse

---
+++ Bravo/Company.swift
@@ -0,0 +1,16 @@
+//
+//  Company.swift
+//  Bravo
+//
+//  Created by Patwardhan, Saurabh on 11/15/16.
+//  Copyright © 2016 BravoInc. All rights reserved.
+//
+
+import UIKit
+import Parse
+class Company: PFObject {
+    var name : String?
+    var teams : [Team]?
+    
+    
+}
