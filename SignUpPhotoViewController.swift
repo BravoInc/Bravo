9fa66612326653f4579fd3962c7f84e10d0224f2
Jay Liew <jaysern@gmail.com>
2016-12-02 16:41:44 -0800

    added screen in storyboard for signup profile photo

---
+++ SignUpPhotoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SignUpPhotoViewController.swift
+//  Bravo
+//
+//  Created by Jay Liew on 12/2/16.
+//  Copyright © 2016 BravoInc. All rights reserved.
+//
+
+import UIKit
+
+class SignUpPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        miscInit()
+    }
+    
+    func miscInit(){
+        UIApplication.shared.statusBarStyle = .lightContent
+        
+        //emailTextField.becomeFirstResponder()
+        
+        let button: UIButton = UIButton(type: UIButtonType.custom)
+        //set image for button
+        let backImage = UIImage(named: "backArrow128.png")!
+        button.setImage(backImage, for: UIControlState.normal)
+        //add function for button
+        button.addTarget(self, action: #selector(SignUpPhotoViewController.backButtonPressed), for: UIControlEvents.touchUpInside)
+        //set frame
+        button.frame = CGRect(x: 0, y: 0, width: 15, height: 15)
+        
+        let barButton = UIBarButtonItem(customView: button)
+        //assign button to navigationbar
+        self.navigationItem.leftBarButtonItem = barButton
+    }
+    
+    func backButtonPressed() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
