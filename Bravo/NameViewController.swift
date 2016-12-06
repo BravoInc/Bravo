1702669a29108f9a899dc960aa394ad6c5b02f31
Jay Liew <jaysern@gmail.com>
2016-12-05 21:16:40 -0800

    FB login button on the account creation VC

--- Bravo/NameViewController.swift
+++ Bravo/NameViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+import FBSDKCoreKit
+import ParseFacebookUtilsV4
+
 struct SignUpUser {
     var firstName: String?
     var lastName: String?
@@ -28,6 +31,119 @@ class NameViewController: UIViewController {
         super.viewDidLoad()
         miscInit()
     }
+    
+    @IBAction func onFBTap(_ sender: Any) {
+        
+        PFFacebookUtils.logInInBackground(withReadPermissions: ["public_profile", "email", "user_friends"],
+                                          block: {
+                                            (user: PFUser?, error: Error?) -> Void in
+                                            if let user = user
+                                            {
+                                                if user.isNew {
+                                                    print("User signed up and logged in through Facebook!")
+                                                } else {
+                                                    print("User logged in through Facebook!")
+                                                }
+                                                
+                                                if let installation = PFInstallation.current() {
+                                                    installation["user"] = PFUser.current()
+                                                    installation.saveEventually()
+                                                }
+                                                
+                                                
+                                                
+                                                let graphRequest:FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"first_name, last_name, id, email, picture.type(large)"])
+                                                
+                                                graphRequest.start(completionHandler: { (connection, result, error) -> Void in
+                                                    
+                                                    print("--- GRAPH REQUEST START")
+                                                    
+                                                    if ((error) != nil){
+                                                        print("Error: \(error?.localizedDescription)")
+                                                    }
+                                                    else {
+                                                        // success
+                                                        
+                                                        print("--- GRAPH REQUEST SUCCESS")
+                                                        
+                                                        var userData: [String: AnyObject] = result as! [String : AnyObject]
+                                                        
+                                                        let picture: [String: AnyObject] = userData["picture"] as! [String: AnyObject]
+                                                        
+                                                        let imageData: [String: AnyObject] = picture["data"] as! [String : AnyObject]
+                                                        
+                                                        
+                                                        if let x = userData["first_name"] as? String {
+                                                            user["firstName"] = x
+                                                        }
+                                                        
+                                                        if let x = userData["last_name"] as? String {
+                                                            user["lastName"] = x
+                                                        }
+                                                        
+                                                        if let x = userData["email"] as? String {
+                                                            user["email"] = x
+                                                        }
+                                                        
+                                                        print("--- \(user["firstName"]) ||| \(user["lastName"]) +++ \(user["email"])")
+                                                        
+                                                        if let x = imageData["url"] as? String {
+                                                            user["fbProfilePhotoUrl"] = x
+                                                            print("--- FB IMAGE DATA: \(x)")
+                                                            
+                                                            let url = URL(string: x)!
+                                                            DispatchQueue.global().async {
+                                                                let data = try? Data(contentsOf: url) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+                                                                DispatchQueue.main.async {
+                                                                    let photoFromUrl = UIImage(data: data!)
+                                                                    
+                                                                    if let photo = photoFromUrl {
+                                                                        let imageData = UIImagePNGRepresentation(photo)
+                                                                        let imageFile = PFFile(name:"image.png", data:imageData!)
+                                                                        
+                                                                        user["profileImage"] = imageFile
+                                                                        
+                                                                        user.saveInBackground(block: { (succeeded: Bool, error:Error?) in
+                                                                            if(succeeded == true){
+                                                                                print("--- new profile photo upload OK")
+                                                                            }else{
+                                                                                print("---!!! new profile photo upload FAIL")
+                                                                            }
+                                                                            
+                                                                            if let e = error{
+                                                                                print("---!!! new profile photo upload: \(e.localizedDescription)")
+                                                                            }
+                                                                        }) // save in bg
+                                                                        
+                                                                    } // if photo
+                                                                    
+                                                                }
+                                                            } // global dispatch
+                                                            
+                                                        }
+                                                        
+                                                        //print("--- IMAGE DATA URL FROM FB: \(imageData["url"])")
+                                                        
+                                                        user.saveInBackground(block: { (result: Bool, error: Error?) in
+                                                            if result {
+                                                                print("--- Successfully FB log ina and saved data")
+                                                            }
+                                                            if error != nil {
+                                                                print("Error: \(error?.localizedDescription)")
+                                                            }
+                                                            
+                                                        })
+                                                    }
+                                                }) // graphRequest
+                                                
+                                                self.present(afterSuccessLogin(), animated: true, completion: nil)
+                                                
+                                            } else {
+                                                print("Uh oh. The user cancelled the Facebook login.")
+                                            } // else
+        } //block
+        )
+    } // fb tap
     
     @IBAction func onTap(_ sender: Any) {
         signUpUser.firstName = firstNameTextField.text
