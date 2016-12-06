9237aee1a9b48679164c55322eacfd427937c842
Jay Liew <jaysern@gmail.com>
2016-12-05 21:00:11 -0800

    fb login UI on login view

--- Bravo/Common.swift
+++ Bravo/Common.swift
@@ -10,6 +10,51 @@ import Foundation
 import Parse
 import SCLAlertView
 
+func afterSuccessLogin() -> UITabBarController{
+    // if you change things here, don't forget to change things in the success callback of the new account signup
+    
+    print("--- LOGIN success")
+    let storyBoard = UIStoryboard(name: "Activity", bundle: nil)
+    
+    let defaults = UserDefaults.standard
+    if let deviceTokenString = defaults.object(forKey: "deviceTokenString") as? String {
+        // if user permitted push notifications, save deviceTokenString to their profile
+        let user = PFUser.current()
+        user?["deviceTokenString"] = deviceTokenString
+        user?.saveInBackground()
+    }
+    
+    let timelineNavigationController = storyBoard.instantiateViewController(withIdentifier: "TimelineNavigationController") as! UINavigationController
+    let timelineViewController = timelineNavigationController.topViewController as! TimelineViewController
+    timelineNavigationController.tabBarItem.title = "Timeline"
+    //timelineNavigationController.tabBarItem.image = UIImage(named: "NoImage")
+    
+    
+    let storyBoardTC = UIStoryboard(name: "TeamCreation", bundle: nil)
+    let teamNavigationController = storyBoardTC.instantiateViewController(withIdentifier: "TeamNavigationController") as! UINavigationController
+    //let teamViewController = teamNavigationController.topViewController as! TeamViewController
+    teamNavigationController.tabBarItem.title = "Teams"
+    //teamNavigationController.tabBarItem.image = UIImage(named: "NoImage")
+    
+    let leaderboardNavigationController = storyBoard.instantiateViewController(withIdentifier: "LeaderboardNavigationController") as! UINavigationController
+    let leaderboardViewController = leaderboardNavigationController.topViewController as! LeaderboardViewController
+    leaderboardNavigationController.tabBarItem.title = "Leaderboard"
+    //leaderboardNavigationController.tabBarItem.image = UIImage(named: "NoImage")
+    
+    let profileNavigationController = storyBoard.instantiateViewController(withIdentifier: "ProfileNavigationController") as! UINavigationController
+    let profileViewController = profileNavigationController.topViewController as! ProfileViewController
+    profileNavigationController.tabBarItem.title = "Profile"
+    //profileNavigationController.tabBarItem.image = UIImage(named: "NoImage")
+    
+    let tabBarController = UITabBarController()
+    tabBarController.viewControllers = [timelineNavigationController, teamNavigationController, leaderboardNavigationController, profileNavigationController]
+    //tabBarController.selectedViewController = teamNavigationController
+    
+    return tabBarController
+    
+} // after success login
+
+
 func sendPushNotification(recipient: PFUser, message: String) -> Void {
     
     guard recipient["deviceTokenString"] != nil && message != "" else {
