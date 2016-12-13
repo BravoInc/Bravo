385efe058cb59ba0d6d7c69ca2911c5b77dc1ffb
Jay Liew <jaysern@gmail.com>
2016-12-13 03:39:20 -0800

    Walkthough view controllers

---
+++ Bravo/CustomWalkthroughViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MyCustomPageViewController.swift
+//  TB_Walkthrough
+//
+//  Created by Yari D'areglia on 12/03/16.
+//  Copyright © 2016 Bitwaker. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomPageViewController: BWWalkthroughPageViewController {
+    
+    //@IBOutlet var backgroundView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.layer.zPosition = -1000
+        view.layer.isDoubleSided = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //self.backgroundView.layer.masksToBounds = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func walkthroughDidScroll(position: CGFloat, offset: CGFloat) {
+        var tr = CATransform3DIdentity
+        tr.m34 = -1/1000.0
+        view.layer.transform = CATransform3DRotate(tr, CGFloat(M_PI)  * (1.0 - offset), 0.5,1, 0.2)
+    }
+    
+
+}
